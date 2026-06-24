a74c3f4a64499ad7a8798e767578b9502ebc41d7
sakshi0528 <sakshimaurya60920@gmail.com>
2026-06-24 19:37:15 +0530

    Premium UI Upgrade

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ document.body.classList.toggle("light");
 
 });
 
+const text =
+"Aspiring SOC Analyst | Cybersecurity Enthusiast | Networking Learner";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,50);
+
+}
+
+}
+
+typing();
